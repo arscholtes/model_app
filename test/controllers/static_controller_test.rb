f16ebcfa2c6c46b52f8a8a3b8c6536842792b95d
Th3MFG <arscholtes@gmail.com>
2021-03-14 22:31:10 -0400

    night time

---
+++ test/controllers/static_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class StaticControllerTest < ActionDispatch::IntegrationTest
+  test "should get pages" do
+    get static_pages_url
+    assert_response :success
+  end
+
+  test "should get ..." do
+    get static_..._url
+    assert_response :success
+  end
+end
