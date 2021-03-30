d5677a7afa70363a336f751a49170aafc2ca5fd3
Th3MFG <arscholtes@gmail.com>
2021-03-30 18:44:10 -0400

    pushing changes for static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,15 @@
-require "test_helper"
-
+require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
+  test "should get root" do
     get static_pages_home_url
     assert_response :success
   end
-
   test "should get help" do
     get static_pages_help_url
     assert_response :success
   end
-
-=begin
   test "should get about" do
     get static_pages_about_url
     assert_response :success
   end
-=end
-
-end
+  end
