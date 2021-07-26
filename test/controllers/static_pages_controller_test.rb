af6899503923f37a24e91231f43f80fdd92b70a4
Th3MFG <arscholtes@gmail.com>
2021-07-26 12:52:41 -0400

    1

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -16,17 +16,16 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
-  #   assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
-  # end
+
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
     assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
-  #   assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
-  # end
-
+   #   assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+   # end
+   # ^ left over from the upgrade so that it will be more dynamic in providing page titles
    # test "should get about" do
    #   get static_pages_help_url
    #   assert_response :success
